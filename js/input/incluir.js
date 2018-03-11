3611412d811ab634c47821f6e56c988e31cff4b3
VictorMFernandes <36870006+VictorMFernandes@users.noreply.github.com>
2018-03-11 20:37:13 -0300

    Consertando Input Incluir

--- js/input/incluir.js
+++ js/input/incluir.js
@@ -1,24 +1,31 @@
 ﻿$(document).ready(function () {
 
+    var nomeIncluir = "Raca";
     var qtdMaxima = 5;
     atualizarStatus()
 
     //Botão Adicionar
     $('.incluirAdicionar').click(function () {
 
-        var num = $('.moldeIncluir').length,
-            newNum = new Number(num + 1),
-            newElem = $('#inputIncluir' + num).clone().attr('id', 'inputIncluir' + newNum).fadeIn('slow');
+        var wrapperAlvo = document.getElementById('incluirWrapper');
 
-        $('#inputIncluir' + num).after(newElem);
+        var qtdAtual = $('.Incluir' + nomeIncluir).length,
+            idASerAtribuido = new Number(qtdAtual + 1),
+            novoElemento = GerarInstancia(idASerAtribuido, nomeIncluir);
+
+        wrapperAlvo.insertAdjacentHTML('beforeend', novoElemento);
+
+        //Ativa o MinMax criado
+        trazerMinMax(document.getElementById('minMaxIncluir' + idASerAtribuido));
 
         //Ativa botão de remover
         $('.incluirRemover').attr('disabled', false);
         //Desativa botão de adicionar
-        if (newNum == qtdMaxima) {
+        if (idASerAtribuido == qtdMaxima) {
             $('.incluirAdicionar').attr('disabled', true).prop('value', "Limite!");
         }
         atualizarStatus()
+
         //Colocar as funções nos controles criados
         $('.incluirRemoverIndividual').click(function () {
             $(this).parent().parent().remove();
@@ -29,11 +36,9 @@
     $('.incluirRemover').click(function () {
         //Caixa de dialogo de confirmação
         if (confirm("Deseja mesmo excluir?")) {
-            var num = $('.moldeIncluir').length;
-
+            var num = $('.Incluir' + nomeIncluir).length;
             $('#inputIncluir' + num).slideUp('slow', function () {
                 $(this).remove();
-
                 //Ativa botão de adicionar
                 $('.incluirAdicionar').attr('disabled', false).prop('value', "Adicionar");
                 //Desativa botão de remover
@@ -50,11 +55,12 @@
     //Botão remover Individual
     $('.incluirRemoverIndividual').click(function () {
         $(this).parent().parent().remove();
+        atualizarStatus();
     });
 
     //Atualiza incluirStatus
     function atualizarStatus() {
-        $('.incluirStatus').val($('.moldeIncluir').length);
+        $('.incluirStatus').val($('.Incluir' + nomeIncluir).length);
     }
 
     // Enable the "add" button
@@ -62,65 +68,53 @@
     // Disable the "remove" button
     $('.incluirRemover').attr('disabled', true);
 
-    //var qtdMaxima = 5;
-    //atualizarStatus()
+    function GerarInstancia(id, nomeDoIncluir) {
+        //Abrindo instância
+        var moldeInputIncluirAbertura = [
+            "<div id='inputIncluir" + id + "' class='form-group col-md-12 Incluir" + nomeDoIncluir + "' style='display:block;'>"
+        ].join("\n");
+        //Molde Texto
+        var moldeTexto = [
+            "<div class='col-sm-11 inputWrapper'>",
+            "<label for='txtIncluir" + id + "' class=col-sm-2 control-label'>Nome</label>",
+            "<div class='inputCorpo col-sm-10'>",
+            "<div class='col-sm-12'>",
+            "<input type='text' class='form-control' id='txtIncluir" + id + "' placeholder='Digite o atributo aqui' />",
+            "</div>",
+            "</div>",
+            "</div>",
+            "<div class='col-sm-1'>",
+            "<span class='button-icon has-bg incluirRemoverIndividual'><i class='fa fa-times'></i></span>",
+            "</div>"
+        ].join("\n");
+        //Molde TextoArea
+        var moldeTextoArea = [
+            "<div class='col-sm-11 inputWrapper'>",
+            "<label for='txtAreaIncluir" + id + "' class='col-sm-2 control-label'>Descrição</label>",
+            "<div class='inputCorpo col-sm-10'>",
+            "<div class='col-sm-12'>",
+            "<textarea id='txtAreaIncluir" + id + "' placeholder='Campo de texto autoexpansível' title='Digite seu texto aqui'></textarea>",
+            "</div>",
+            "</div>",
+            "</div>"
+        ].join("\n");
+        //Molde MinMax
+        var moldeMinMax = [
+            "<div class='col-sm-11 inputWrapper'>",
+            "<label for='minMaxIncluir" + id + "' class='col-sm-2 control-label'>Hostilidade</label>",
+            "<div class='inputCorpo col-sm-10'>",
+            "<div class='col-sm-12'>",
+            "<div id='minMaxIncluir" + id + "' class='minMaxInput mmHosti'></div>",
+            "</div>",
+            "</div>",
+            "</div>"
+        ].join("\n");
 
-    ////Botão Adicionar
-    //$('.incluirAdicionar').click(function () {
+        //Fechando Instância
+        var moldeInputIncluirFechamento = [
+            "</div>"
+        ].join("\n");
 
-    //    var num = $('.moldeIncluir').length,
-    //        newNum = new Number(num + 1),
-    //        newElem = $('#inputIncluir' + num).clone().attr('id', 'inputIncluir' + newNum).fadeIn('slow');
-
-    //    $('#inputIncluir' + num).after(newElem);
-
-    //    //Ativa botão de remover
-    //    $('.incluirRemover').attr('disabled', false);
-    //    //Desativa botão de adicionar
-    //    if (newNum == qtdMaxima) {
-    //        $('.incluirAdicionar').attr('disabled', true).prop('value', "Limite!");
-    //    }
-    //    atualizarStatus()
-    //    //Colocar as funções nos controles criados
-    //    $('.incluirRemoverIndividual').click(function () {
-    //        $(this).parent().parent().remove();
-    //    });
-    //});
-
-    ////Botão Remover
-    //$('.incluirRemover').click(function () {
-    //    //Caixa de dialogo de confirmação
-    //    if (confirm("Deseja mesmo excluir?")) {
-    //        var num = $('.moldeIncluir').length;
-
-    //        $('#inputIncluir' + num).slideUp('slow', function () {
-    //            $(this).remove();
-
-    //            //Ativa botão de adicionar
-    //            $('.incluirAdicionar').attr('disabled', false).prop('value', "Adicionar");
-    //            //Desativa botão de remover
-    //            if ((num - 1) == 0) {
-    //                $('.incluirRemover').attr('disabled', true);
-    //            }
-
-    //            atualizarStatus()
-    //        });
-    //    }
-    //    return false;
-    //});
-
-    ////Botão remover Individual
-    //$('.incluirRemoverIndividual').click(function () {
-    //    $(this).parent().parent().remove();
-    //});
-
-    ////Atualiza incluirStatus
-    //function atualizarStatus() {
-    //    $('.incluirStatus').val($('.moldeIncluir').length);
-    //}
-
-    //// Enable the "add" button
-    //$('.incluirAdicionar').attr('disabled', false);
-    //// Disable the "remove" button
-    //$('.incluirRemover').attr('disabled', true);
+        return [moldeInputIncluirAbertura, moldeTexto, moldeTextoArea, moldeMinMax, moldeInputIncluirFechamento].join("\n");
+    }
 });
--- js/input/minMax.js
+++ js/input/minMax.js
@@ -2,32 +2,29 @@
 
     //Tratamento de todos os inputs do tipo MinMax 
     $(".minMaxInput").each(function () {
-
-        var valores;
-
-        //Acrescentar cases para cada id de input MinMax
-        switch (this.id) {
-            case "idMinMaxHosti":
-                valores = ["Inofensivos", "Pacíficos", "Neutros", "Ameaçadores", "Agressivos"];
-                break;
-
-            case "idMinMaxAltura":
-                valores = ["Nanica", "Baixa", "Média", "Alta", "Gigante"];
-                break;
-
-            case "idMinMaxCorpo":
-                valores = ["Raquítico", "Magro", "Normal", "Gordo", "Obeso"];
-                break;
-        }
-
-        //Cria o MinMax no objeto atual, não modificar
-        $(this).ionRangeSlider({
-            from: 2,
-            values: valores,
-            type: 'single',
-            hasGrid: false
-        });
-
+        trazerMinMax(this)
     });
-
 });
+
+function trazerMinMax(objeto) {
+
+    var valores;
+
+    if (objeto.className.indexOf("mmHosti") != -1) {
+        valores = ["Inofensivos", "Pacíficos", "Neutros", "Ameaçadores", "Agressivos"];
+    }
+    else if (objeto.className.indexOf("mmAltura") != -1) {
+        valores = ["Nanica", "Baixa", "Média", "Alta", "Gigante"];
+    }
+    else if (objeto.className.indexOf("mmCorpo") != -1) {
+        valores = ["Raquítico", "Magro", "Normal", "Gordo", "Obeso"];
+    }
+
+    //Cria o MinMax no objeto atual, não modificar
+    $(objeto).ionRangeSlider({
+        from: 2,
+        values: valores,
+        type: 'single',
+        hasGrid: false
+    });
+}
