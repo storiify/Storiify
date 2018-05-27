1b3688b3f47d87bb999f3ef1aa2c553f48527ece
VictorMFernandes <36870006+VictorMFernandes@users.noreply.github.com>
2018-05-27 15:26:59 -0300

    Conserto nome do bd e método duplicado no modleo história

--- app/classes/ConexaoBd.php
+++ app/classes/ConexaoBd.php
@@ -10,7 +10,7 @@ class ConexaoBd {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
-        self::$instance = new PDO('mysql:host=localhost;dbname=storiify', 'root', '', $pdo_options);
+        self::$instance = new PDO('mysql:host=localhost;dbname=bd_storiify', 'root', '', $pdo_options);
         //self::$instance = new PDO('mysql:host=localhost;dbname=bd_storiify', 'root', '', $pdo_options);
       }
       return self::$instance;
