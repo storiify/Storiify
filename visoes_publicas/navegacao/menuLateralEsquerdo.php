fdfff411cbf8e0c375dfbace5a3ee4a2a242745e
VictorMFernandes <36870006+VictorMFernandes@users.noreply.github.com>
2018-05-29 11:33:53 -0300

    Exclusão de mundo do menu lateral

--- visoes_publicas/navegacao/menuLateralEsquerdo.php
+++ visoes_publicas/navegacao/menuLateralEsquerdo.php
@@ -89,30 +89,6 @@ $historias = sessao()->getHistoriasData();
 
         <li class="nav-item list-group-item">
             <i class="fa fa-globe col-md-2"></i>
-            <span class="col-md-10">Mundo</span>
-            <ul class="navbar-nav mr-auto text-center list-group">
-                <li>
-                    <a href="?categoria=mundo&acao=criar" class="nav-item list-group-item lista-clicavel">
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-8 acao-categoria">Criar Mundo</div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="?categoria=mundo&acao=listar" class="nav-item list-group-item lista-clicavel">
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-8 acao-categoria">Listar Mundo</div>
-                            <div class="col-md-1"><span class="insignia">&nbsp;02&nbsp;</span></div>                        
-                        </div>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
-        <li class="nav-item list-group-item">
-            <i class="fa fa-bank col-md-2"></i>
             <span class="col-md-10">Localização</span>
             <ul class="navbar-nav mr-auto text-center list-group">
                 <li>
@@ -124,7 +100,7 @@ $historias = sessao()->getHistoriasData();
                     </a>
                 </li>
                 <li>
-                    <a href="" class="nav-item list-group-item lista-clicavel">
+                    <a href="?categoria=localizacao&acao=listar" class="nav-item list-group-item lista-clicavel">
                         <div class="row">
                             <div class="col-md-1"></div>
                             <div class="col-md-8 acao-categoria">Listar Localizações</div>
